cc62c327d69b7809ad44a17c51d054d3f697b769
Jorge Lizan Candela <jlizancandela@gmail.com>
2024-07-19 22:05:31 +0200

    problemas solventados

--- src/Journal/Pages/Home.tsx
+++ src/Journal/Pages/Home.tsx
@@ -5,5 +5,5 @@ import { NothingView } from "../views/Nothing";
 export const HomePage = () => {
   const { active } = useAppSelector((state) => state.journal);
 
-  return active ? <NoteView /> : <NothingView />;
+  return active?.id !== undefined ? <NoteView /> : <NothingView />;
 };
--- src/Journal/hooks/useNoteView.tsx
+++ src/Journal/hooks/useNoteView.tsx
@@ -1,46 +1,31 @@
-import { useEffect, useMemo } from "react";
-import { setActiveNote, useAppDispatch, useAppSelector } from "../../store";
+import { useEffect } from "react";
 import { useInput } from "../../auth";
+import { useAppSelector } from "../../store";
 import { dateToString } from "../helpers/dateToString";
 
 export const useNoteView = () => {
   const { active } = useAppSelector((state) => state.journal);
 
-  const dispatch = useAppDispatch();
+  const { values, onChange, reset, newValues } = useInput({
+    title: {
+      value: active?.title || "",
+      error: "",
+    },
+    content: {
+      value: active?.body || "",
+      error: "",
+    },
+  });
 
-  const initialValues = useMemo(() => {
-    return {
-      title: {
-        value: active?.title || "",
-        error: "",
-      },
-      content: {
-        value: active?.body || "",
-        error: "",
-      },
-    };
-  }, [active]);
-
-  const { values, onChange, reset, newValues } = useInput(initialValues);
+  useEffect(() => {
+    reset();
+    console.log("reset");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [active?.id]);
 
   const formattedDate = dateToString(active?.date || 0);
 
-  //Cambia el estado de redux cada vez que cambian los inputs
-  useEffect(() => {
-    dispatch(
-      setActiveNote({
-        id: active?.id || "",
-        date: active?.date || 0,
-        body: values.content.value,
-        title: values.title.value,
-      })
-    );
-  }, [active, dispatch, values]);
-
   //Cambia el estado del hook cada vez que se asigna una nueva nota
-  useEffect(() => {
-    newValues(initialValues);
-  }, [active?.id, initialValues, newValues]);
 
   return { values, onChange, reset, newValues, formattedDate };
 };
